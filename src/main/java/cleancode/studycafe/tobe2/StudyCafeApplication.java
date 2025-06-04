675cf1e9fc26ce331ae552d661193145fdc34f25
yeonjue-2 <ilov-@naver.com>
2025-06-04 22:50:27 +0900

    chore: tobe2를 복제하여 코드 리팩토링 준비

---
+++ src/main/java/cleancode/studycafe/tobe2/StudyCafeApplication.java
@@ -0,0 +1,12 @@
+package cleancode.studycafe.tobe2;
+
+import cleancode.studycafe.tobe.StudyCafePassMachine;
+
+public class StudyCafeApplication {
+
+    public static void main(String[] args) {
+        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine();
+        studyCafePassMachine.run();
+    }
+
+}
---
+++ src/main/java/cleancode/studycafe/tobe2/exception/AppException.java
@@ -0,0 +1,9 @@
+package cleancode.studycafe.tobe2.exception;
+
+public class AppException extends RuntimeException {
+
+    public AppException(String message) {
+        super(message);
+    }
+
+}
