872f07b210f14694f9889c73e92dec3291c7586f
yeonjue-2 <ilov-@naver.com>
2025-06-08 15:17:41 +0900

    fix: 열렸거나 깃발이 꽂혀있으면 정확한 지뢰를 찾지 않아도 게임에 승리하는 버그 수정

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -3,14 +3,14 @@ package cleancode.minesweeper.tobe;
 
 import cleancode.minesweeper.tobe.minesweeper.Minesweeper;
 import cleancode.minesweeper.tobe.minesweeper.config.GameConfig;
-import cleancode.minesweeper.tobe.minesweeper.gamelevel.Beginner;
+import cleancode.minesweeper.tobe.minesweeper.gamelevel.VeryBeginner;
 import cleancode.minesweeper.tobe.minesweeper.io.ConsoleInputHandler;
 import cleancode.minesweeper.tobe.minesweeper.io.ConsoleOutputHandler;
 
 public class GameApplication {
     public static void main(String[] args) {
         GameConfig gameConfig = new GameConfig(
-                new Beginner(),
+                new VeryBeginner(),
                 new ConsoleInputHandler(),
                 new ConsoleOutputHandler()
         );
--- src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/EmptyCell.java
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/EmptyCell.java
@@ -38,7 +38,7 @@ public class EmptyCell implements Cell {
 
     @Override
     public boolean isChecked() {
-        return cellState.isChecked();
+        return cellState.isOpened();
     }
 
     @Override
